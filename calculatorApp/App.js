5cda12de05004420cfee1b36dc10ae5a8cb2c67b
Clark <admin@repitsocial.com>
2022-08-16 19:28:44 +0800

    calculator app

---
+++ calculatorApp/App.js
@@ -0,0 +1,59 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+} from 'react-native';
+
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => Node = () => {
+  const backgroundStyle = {
+    backgroundColor: Colors.darker,
+  };
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={backgroundStyle}>
+          <Header />
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
---
+++ calculatorApp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {LogBox, Text} from 'react-native';
+
+LogBox.ignoreAllLogs();
+
+const App = () => {
+  return (
+    <>
+      <Text>here</Text>
+    </>
+  );
+};
+
+export default App;
